2db2d991bfdec6680bddbb003cb5d8154ac3e5d0
Alex Reynolds <hellport@protonmail.com>
2024-07-16 11:21:39 -0300

    solved using prefix/postfix arrays

--- array/array_test.go
+++ array/array_test.go
@@ -97,3 +97,25 @@ func TestTopKFrequentElements(t *testing.T) {
 		})
 	}
 }
+
+func TestProductOfArrayExceptSelf(t *testing.T) {
+	testCases := []struct {
+		name     string
+		nums     []int
+		expected []int
+	}{
+		{name: "1", nums: []int{1, 2, 3, 4}, expected: []int{24, 12, 8, 6}},
+		{name: "2", nums: []int{-1, 1, 0, -3, 3}, expected: []int{0, 0, 9, 0, 0}},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ProductOfArrayExceptSelf(tt.nums)
+			want := tt.expected
+
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v want %v", got, want)
+			}
+		})
+	}
+}
---
+++ array/product_of_array_except_self.go
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/product-of-array-except-self/description/
+
+package leetcode
+
+func ProductOfArrayExceptSelf(nums []int) []int {
+	// Get the values for the prefix array
+	prefix := make([]int, len(nums))
+	for i, num := range nums {
+		// guard this
+		if i == 0 {
+			prefix[i] = 1 * num
+		} else {
+			prefix[i] = prefix[i-1] * num
+		}
+	}
+
+	// Get the values for the postfix array
+	postfix := make([]int, len(nums))
+	for i := len(nums) - 1; i >= 0; i-- {
+		if i == len(nums)-1 {
+			postfix[i] = 1 * nums[i]
+		} else {
+			postfix[i] = postfix[i+1] * nums[i]
+		}
+	}
+
+	// Combine the prefix array with the postfix array
+	result := make([]int, len(nums))
+	for i := range nums {
+		if i == 0 {
+			result[i] = 1 * postfix[i+1]
+		} else if i == len(nums)-1 {
+			result[i] = prefix[i-1] * 1
+		} else {
+			result[i] = prefix[i-1] * postfix[i+1]
+		}
+	}
+
+	return result
+}
