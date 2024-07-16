bb108c40bbc763e9f5ba2358d21793a0b2cf38c7
Alex Reynolds <hellport@protonmail.com>
2024-07-16 19:14:31 -0300

    solved using a map as a set

---
+++ array/longest_consecutive_sequence.go
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+package leetcode
+
+func LongestConsecutiveSequence(nums []int) int {
+	maxStreak := 0
+
+	// Build a set of unique values
+	uniqueMap := make(map[int]bool)
+
+	for _, num := range nums {
+		if _, ok := uniqueMap[num]; !ok {
+			uniqueMap[num] = true
+		}
+	}
+
+	// Find the start of a sequence
+	for key := range uniqueMap {
+		if _, ok := uniqueMap[key-1]; ok {
+			continue
+		}
+
+		// Continue counting the streak until num is not in sequence
+		streak := 0
+		for {
+			if _, ok := uniqueMap[key+streak]; ok {
+				streak++
+			} else {
+				maxStreak = max(streak, maxStreak)
+				break
+			}
+		}
+	}
+
+	return maxStreak
+}
