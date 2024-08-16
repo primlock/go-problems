8e08a18cde82e6ac93850eaa4bfe41685901da65
Alex Reynolds <hellport@protonmail.com>
2024-08-16 12:58:55 -0300

    solved rotate array with rotation algorithm

--- array/array_test.go
+++ array/array_test.go
@@ -141,3 +141,25 @@ func TestLongestConsecutiveSequence(t *testing.T) {
 		})
 	}
 }
+
+func TestRotateArray(t *testing.T) {
+	testCases := []struct {
+		name     string
+		nums     []int
+		k        int
+		expected []int
+	}{
+		{name: "1", nums: []int{1, 2, 3, 4, 5, 6, 7}, k: 3, expected: []int{5, 6, 7, 1, 2, 3, 4}},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Rotate(tt.nums, tt.k)
+			want := tt.expected
+
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v want %v", got, want)
+			}
+		})
+	}
+}
---
+++ array/rotate_array.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func Rotate(nums []int, k int) []int {
+	n := len(nums)
+	rotated := []int{}
+
+	// find the minimum number of rotations
+	k = k % n
+
+	// translate the left rotatios into right rotations
+	rotations := n - k
+
+	// add from k to the end into the new array
+	for i := rotations; i < n; i++ {
+		rotated = append(rotated, nums[i])
+	}
+
+	// add from 0 to k in the new array
+	for i := 0; i < rotations; i++ {
+		rotated = append(rotated, nums[i])
+	}
+
+	return rotated
+}
