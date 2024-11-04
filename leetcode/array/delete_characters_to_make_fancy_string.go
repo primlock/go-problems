cf2bd572227de0098bb664225a8185b98fc4ea3d
Alex Reynolds <hellport@protonmail.com>
2024-11-04 14:36:11 -0400

    add missing description

--- leetcode/array/delete_characters_to_make_fancy_string.go
+++ leetcode/array/delete_characters_to_make_fancy_string.go
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/delete-characters-to-make-fancy-string
+
 package array
 
 import "strings"
@@ -6,12 +8,14 @@ func MakeFancyString(s string) string {
 	count := 1
 	result := []string{}
 
+	// Append the character occurences that are less than 3 to a string slice
 	for i, char := range s {
 		if i == 0 {
 			result = append(result, string(char))
 			continue
 		}
 
+		// If this character is the same as the last
 		if char == rune(s[i-1]) {
 			count++
 			if count < 3 {
