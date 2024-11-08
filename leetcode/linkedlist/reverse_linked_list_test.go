cddaf58e54a6655e43d4a79f70e181e66ed69d0f
Alex Reynolds <hellport@protonmail.com>
2024-11-08 10:24:17 -0400

    add missing test cases

--- leetcode/linkedlist/reverse_linked_list_test.go
+++ leetcode/linkedlist/reverse_linked_list_test.go
@@ -1,1 +1,36 @@
 package linkedlist
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReverseList(t *testing.T) {
+	testCases := []struct {
+		name     string
+		head     *ListNode
+		expected *ListNode
+	}{
+		{
+			name:     "1",
+			head:     &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}}},
+			expected: &ListNode{Val: 5, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: nil}}}}},
+		},
+		{
+			name:     "2",
+			head:     &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: nil}},
+			expected: &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: nil}},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ReverseList(tt.head)
+			want := tt.expected
+
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v, want %v", got, want)
+			}
+		})
+	}
+}
