a8e8ea0bdf80f708831aa1b02772d5f3b11c6de7
Alex Reynolds <hellport@protonmail.com>
2024-11-12 09:13:08 -0400

    helper method to conver an array into a BST

--- leetcode/trees/binary_tree.go
+++ leetcode/trees/binary_tree.go
@@ -5,3 +5,36 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+func ArrayToBST(nodes []interface{}) *TreeNode {
+	if len(nodes) == 0 {
+		return nil
+	}
+
+	// Create the root node from the first element
+	root := &TreeNode{Val: nodes[0].(int)}
+	queue := []*TreeNode{root}
+	i := 1
+
+	// Process the queue and insert nodes
+	for i < len(nodes) && len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		// Left child
+		if i < len(nodes) && nodes[i] != nil {
+			current.Left = &TreeNode{Val: nodes[i].(int)}
+			queue = append(queue, current.Left)
+		}
+		i++
+
+		// Right child
+		if i < len(nodes) && nodes[i] != nil {
+			current.Right = &TreeNode{Val: nodes[i].(int)}
+			queue = append(queue, current.Right)
+		}
+		i++
+	}
+
+	return root
+}
